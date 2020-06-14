5abbe1771d03e7a2a70cdfc464e84281307d19f6
Noel-H <noel.huberto@gmail.com>
2020-06-15 00:42:37 +0200

    Compare les occurences et les enregistres dans un fichiers trier dans l'ordre alphabetique

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,35 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("Project02Eclipse\\symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		//Création de collection
+		List<String> lineListe = new ArrayList<>();
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
+			lineListe.add(line);
+			line = reader.readLine();
 		}
-		
+		// Tri de la collection dans l'ordre alphabetique en enlevant les doublons
+		TreeSet lineListeWithoutDoublonTreeset = new TreeSet(lineListe);
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+		Iterator it = lineListeWithoutDoublonTreeset.iterator();
+		while (it.hasNext()){
+			Object s = it.next();
+			writer.write(s + " = " + Collections.frequency(lineListe, s));
+			if(it.hasNext()){
+				writer.write("\n");
+			}
+		}
 		writer.close();
 	}
-}
+	}
+
